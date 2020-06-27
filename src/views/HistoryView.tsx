3b46466fe474b573ee1e636bb6e49906dd39b184
MaksimKotau <maxkotov@gmail.com>
2020-06-27 08:30:45 -0400

    Correction HistoryView

--- src/views/HistoryView.tsx
+++ src/views/HistoryView.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import SwipableList, { SwipableListElement } from '../components/SwipableList';
+import { HistoryData, removeFromHistory, useStore } from '../state/state';
+import CalculationType from '../enums/calculationType';
 
 const HistoryView: React.FC<{}> = () => {
+    const { state, dispatch } = useStore();
+    const renderElement = (el: HistoryData) => {
+        return (
+            <>
+                <Typography variant="subtitle2">{`Date: ${new Date(el.date).toLocaleString()}`}</Typography>
+                <Typography variant="subtitle2">{`Type: ${CalculationType[el.type]}`}</Typography>
+            </>
+        )
+    }
+    const elements: SwipableListElement[] = state.history.map(el => ({
+        id: el.date,
+        swipeAction: () => dispatch(removeFromHistory(el.date)),
+        renderFunction: () => renderElement(el)
+    }));
     return(
-        <>
-        HistoryView
-        </>
+        <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            <SwipableList 
+                elements={elements}
+            />
+        </div>
     )
 }
 
