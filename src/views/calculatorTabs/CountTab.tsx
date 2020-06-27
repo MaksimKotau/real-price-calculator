0955659be217e2cf566f9181c1edf3027dfcad0b
MaksimKotau <maxkotov@gmail.com>
2020-06-27 08:26:24 -0400

    Correction CountTab

--- src/views/calculatorTabs/CountTab.tsx
+++ src/views/calculatorTabs/CountTab.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import React, { useState } from 'react';
+import AddButton from '../../components/AddButton';
+import SwipableList, { SwipableListElement } from '../../components/SwipableList';
+import { CountCompareData, removeDataFromCountAndSaveToHistory, useStore } from '../../state/state';
+import CountForm from './CountForm';
 
 const CountTab: React.FC<{}> = () => {
+    const [isFormOpen, setFormOpen] = useState<boolean>(false);
+    const { state, dispatch } = useStore();
+    const renderElement = (el: CountCompareData) => {
+        return (
+            <>
+                <Typography variant="subtitle2">{`Count: ${el.count}`}</Typography>
+                <Typography variant="subtitle2">{`Price: ${el.price}`}</Typography>
+                <Typography variant="subtitle2">{`REAL PRICE FOR 1 UNIT: ${getPriceBy1Unit(el.count, el.price)}`}</Typography>
+            </>
+        )
+    }
+    const elements: SwipableListElement[] = state.calculation.count.map(el => ({
+        id: el.id,
+        swipeAction: () => removeDataFromCountAndSaveToHistory(dispatch, el.id),
+        renderFunction: () => renderElement(el)
+    }));
     return (
-        <>
-            Count tab
-            
-        </>
+        <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            <SwipableList
+                elements={elements}
+            />
+            <AddButton onClick={() => setFormOpen(true)} />
+            <CountForm
+                isOpen={isFormOpen}
+                handleClose={() => setFormOpen(false)}
+            />
+        </div>
     )
 }
 
 export default CountTab;
+
+export const getPriceBy1Unit = (count: number, price: number): number => {
+    const priceForOneUnit = price / count;
+    return priceForOneUnit;
+}
