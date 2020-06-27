cceafb13aa058095174c77f00fed975d78b91e5c
MaksimKotau <maxkotov@gmail.com>
2020-06-27 08:26:54 -0400

    Correction WeightTab

--- src/views/calculatorTabs/WeightTab.tsx
+++ src/views/calculatorTabs/WeightTab.tsx
@@ -1,63 +1,35 @@
+import Typography from '@material-ui/core/Typography';
 import React, { useState } from 'react';
 import AddButton from '../../components/AddButton';
-import WeightForm from './WeightForm';
-import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
-import { withLocalStorageCache, getInitialState } from '../../state/utils';
-import { appReducer, ApplicationState } from '../../state/state';
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import createStyles from '@material-ui/core/styles/createStyles';
-import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import SwipableList, { SwipableListElement } from '../../components/SwipableList';
 import WeightType from '../../enums/weightType';
 import { convertWeight } from '../../service/index';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        cardsContainer: {
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            boxSizing: "border-box",
-            overflowY: "auto",
-            position: "absolute"
-        },
-        cardContainer: {
-            width: "100%",
-            padding: 10,
-            boxSizing: "border-box"
-        }
-    }));
+import { removeDataFromWeightAndSaveToHistory, useStore, WeightCompareData } from '../../state/state';
+import WeightForm from './WeightForm';
 
 const WeightTab: React.FC<{}> = () => {
-    const classes = useStyles();
     const [isFormOpen, setFormOpen] = useState<boolean>(false);
-    const [state, dispatch] = React.useReducer(
-        withLocalStorageCache(appReducer),
-        getInitialState()
-    )
-    const getPriceBy100g = (unitType: WeightType, count: number, price: number): number => {
-        const priceForOneUnit = price / count;
-        const conversionCoefficient = convertWeight(WeightType.gram, unitType, 1);
-        const priceFor_1g = priceForOneUnit * conversionCoefficient;
-        return priceFor_1g * 100;
+    const { state, dispatch } = useStore();
+    const renderElement = (el: WeightCompareData) => {
+        return (
+            <>
+                <Typography variant="subtitle2">{`Units: ${el.unitType}`}</Typography>
+                <Typography variant="subtitle2">{`Weight: ${el.count}`}</Typography>
+                <Typography variant="subtitle2">{`Price: ${el.price}`}</Typography>
+                <Typography variant="subtitle2">{`REAL PRICE FOR 100g: ${getPriceBy100g(el.unitType, el.count, el.price)}`}</Typography>
+            </>
+        )
     }
+    const elements: SwipableListElement[] = state.calculation.weight.map(el => ({
+        id: el.id,
+        swipeAction: () => removeDataFromWeightAndSaveToHistory(dispatch, el.id),
+        renderFunction: () => renderElement(el)
+    }));
     return (
         <div style={{ width: "100%", height: "100%", position: "relative" }}>
-            <div className={classes.cardsContainer}>
-                {(state as ApplicationState).calculation.weight.map((el, index) => {
-                    return (
-                        <div className={classes.cardContainer}>
-                            <Paper>
-                                <Typography variant="subtitle2">{`Units: ${el.unitType}`}</Typography>
-                                <Typography variant="subtitle2">{`Weight: ${el.count}`}</Typography>
-                                <Typography variant="subtitle2">{`Price: ${el.price}`}</Typography>
-                                <Typography variant="subtitle2">{`REAL PRICE FOR 100g: ${getPriceBy100g(el.unitType, el.count, el.price)}`}</Typography>
-                            </Paper>
-                        </div>
-                    );
-                })}
-            </div>
+            <SwipableList
+                elements={elements}
+            />
             <AddButton onClick={() => setFormOpen(true)} />
             <WeightForm
                 isOpen={isFormOpen}
@@ -68,3 +40,10 @@ const WeightTab: React.FC<{}> = () => {
 }
 
 export default WeightTab;
+
+export const getPriceBy100g = (unitType: WeightType, count: number, price: number): number => {
+    const priceForOneUnit = price / count;
+    const conversionCoefficient = convertWeight(WeightType.gram, unitType, 1);
+    const priceFor_1g = priceForOneUnit * conversionCoefficient;
+    return priceFor_1g * 100;
+}
